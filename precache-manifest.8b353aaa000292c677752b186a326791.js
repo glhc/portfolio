e520d02c5985249af32573f5d946dc42c16ed12e
Patrick Horne <patrickahorne@gmail.com>
2020-02-26 02:26:43 +1100

    Updates

---
+++ precache-manifest.8b353aaa000292c677752b186a326791.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f8f1e7cd0172106d67e6dc426b8bdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3e15c9b9881883e3c23",
+    "url": "/static/css/main.ac38835c.chunk.css"
+  },
+  {
+    "revision": "ad8e8dccf92391a62d32",
+    "url": "/static/js/2.50fdd86e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.50fdd86e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3e15c9b9881883e3c23",
+    "url": "/static/js/main.c1cc2f2f.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
