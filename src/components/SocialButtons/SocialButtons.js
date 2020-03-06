0bc7265b06034ee28cdf4d87afb8336bf248310c
Patrick Horne <patrickahorne@gmail.com>
2020-03-06 12:10:19 +1100

    Update social buttons on landing

--- src/components/SocialButtons/SocialButtons.js
+++ src/components/SocialButtons/SocialButtons.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import StyledSocialButtons from './StyledSocialButtons';
 
 export default function SocialButtons() {
   return (
-    <aside class="social-buttons">
-      <ion-icon name="logo-github"></ion-icon>
-      <ion-icon name="logo-twitter"></ion-icon>
-      <ion-icon name="logo-linkedin"></ion-icon>
-      <ion-icon name="mail-outline"></ion-icon>
-    </aside>
+    <StyledSocialButtons>
+      <div class="social-buttons">
+        <ion-icon name="logo-github"></ion-icon>
+        <ion-icon name="logo-twitter"></ion-icon>
+        <ion-icon name="logo-linkedin"></ion-icon>
+        <ion-icon name="mail"></ion-icon>
+      </div>
+      <div class="spacer-right"></div>
+    </StyledSocialButtons>
   );
 };
--- src/components/SocialButtons/StyledSocialButtons.js
+++ src/components/SocialButtons/StyledSocialButtons.js
@@ -1,9 +1,20 @@
 import * as solarized from 'solarized-colors';
 import styled from 'styled-components';
-import SocialButtons from './SocialButtons';
 
-const StyledSocialButtons = styled.aside`
-  border: solid red 2px;
+const StyledSocialButtons = styled.header`
+  margin-top: 8px;
+  font-size: 2.2rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  ion-icon {
+    margin-left: 0.4rem;
+    margin-right: 0.4rem;
+  }
+
+  .social-buttons {
+    margin-right: 4rem;
+  }
 `;
 
 export default StyledSocialButtons;
