ff5e58926716935a6b8f121d3a3511fc3c472272
Patrick Horne <patrickahorne@gmail.com>
2020-03-09 18:07:48 +1100

    Update some stuff

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import Button from '../Button/Button';
+import StyledLanding from './LandingStyles';
 
 export default function Landing() {
 
   return (
-    <>
-      <section class="landing">
-        <article class="text">
+    <StyledLanding>
+      <section className="landing">
+        <aside className="social-icons">
+          <ion-icon name="logo-github"></ion-icon>
+          <ion-icon name="logo-linkedin"></ion-icon>
+          <ion-icon name="logo-twitter"></ion-icon>
+        </aside>
+        <article className="text">
           <div>
-            Hi, I'm <wbr/><span class="accent" id="name">Patrick Horne</span>.<wbr/>
+            Hi, I'm <wbr/><span className="accent" id="name">Patrick Horne</span>.<wbr/>
           </div>
           I'm an aspiring full-stack web developer.
         </article>
 
         <Button text="See more" icon="keyboard_arrow_down"/>
       </section>
-    </>
+    </StyledLanding>
   );
 };
 
--- src/components/Landing/LandingStyles.js
+++ src/components/Landing/LandingStyles.js
@@ -1,6 +1,67 @@
 import styled from 'styled-components';
+import '../../assets/styles/solarized';
 
 const Landing = styled.section`
+  .landing {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    justify-content: center;
+    align-items: center;
+    
+    font-family: "Spartan";
+    font-size: 2rem;
+    font-weight: extra-bold;
+
+    grid-template-rows: 3fr 1fr 1fr 3fr;
+    
+    .text {
+      display: inherit;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      flex-grow: 100;
+    }
+
+    /* Put the sentences in the middle */
+    p {
+      display: flex;
+    }
+    p:nth-child(1) {
+      grid-row: 2 / 3;
+      /* fix name not respecting leading space character */
+    }
+    p:nth-child(2) {
+      grid-row: 3 / 4;
+    }
+
+    button {
+      background-color: $base03;
+      border: solid ${magenta} 2px;
+      border-radius: 1px;
+      font-size: 20px;
+      font-weight: 400;
+      color: $magenta;
+
+
+      display: flex;
+      flex-direction: row;
+      align-self: center;
+      justify-self: end;
+
+      i {
+        color: $magenta;
+        font-size: 33px;
+      }
+      
+    }
+
+    button:focus {
+      outline: none;
+    }
+    margin-bottom: 10px;
+}
 `;
 
 export default Landing;
